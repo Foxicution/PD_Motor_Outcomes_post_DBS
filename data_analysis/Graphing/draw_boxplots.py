f541cd95e34c4730244e361967b359eb37c1ae58
Foxicution <62815446+Foxicution@users.noreply.github.com>
2022-04-01 15:22:17 +0300

    Remove old code
    Remove old code

--- data_analysis/Graphing/draw_boxplots.py
+++ data_analysis/Graphing/draw_boxplots.py
@@ -6,7 +6,6 @@ df = pd.read_csv("MRMR_2.csv", index_col=0)
 df1 = pd.read_csv("G1.csv", index_col=0, usecols =[i for i in range(23)])
 df2 = pd.read_csv("G2.csv", index_col=0, usecols =[i for i in range(23)])
 
-# fig, axes = plt.subplots(5,4, figsize=(15, 20))
 fig, axes = plt.subplots(5,4, figsize=(15, 20))
 
 for i,el in enumerate(list(df.columns.values)[:-1]):
@@ -17,14 +16,5 @@ for i,el in enumerate(list(df.columns.values)[:-1]):
   # remove empty subplot
 plt.tight_layout() 
 plt.suptitle('')
-# fig.delaxes(axes[5,2])
-# fig.delaxes(axes[5,3])
 plt.show()
 fig.savefig('boxplots.tif', dpi=300, bbox_inches='tight', format='tif')
-
-# for i in df.columns:
-#     myFig = plt.figure();
-#     boxplot = df.boxplot(column=[i],
-#                           by='Efektas DBS (1-blogas, 2-geras, 3-labai geras)')
-#     plt.suptitle('')
-#     myFig.savefig('%s.png'%(i), format='png')
